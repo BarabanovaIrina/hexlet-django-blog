ee01a75f0a45bcecfbce1af11da6452745aa230b
BarabanovaIrina <irlania@mail.ru>
2023-03-30 18:51:47 +0500

    Add new `ArticleView` to enable /article/id endpoint
    CHANGELOG:
    - modify `hexlet-django-blog.article.urls.py` to enable
    getting an article by id
    - modify `hexlet-django-blog.article.views.html` by
    adding `ArticleView`
    - modify `hexlet-django-blog.templates.article.index.html` to
    link article name to its page
    - add `hexlet-django-blog.templates.article.show.html` to render
    one article page

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from hexlet_django_blog.article.views import IndexView, CustomView
+from hexlet_django_blog.article.views import (
+    IndexView,
+    CustomView,
+    ArticleView,
+)
 
 urlpatterns = [
-    path("<str:tags>/<int:article_id>/",
-         CustomView.as_view(), name='article'),
     path('', IndexView.as_view()),
+    path('<int:id>/', ArticleView.as_view(), name='article'),
+    path("<str:tags>/<int:article_id>/",
+         CustomView.as_view(), name='article_with_tag'),
 ]
